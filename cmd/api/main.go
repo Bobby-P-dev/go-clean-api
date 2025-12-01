3cd2ee2e90381a13507a73d59da46f7227a20189
Bobby Pratama <bobay772@gmail.com>
2025-12-01 12:43:56 +0700

    first commit

---
+++ cmd/api/main.go
@@ -0,0 +1,35 @@
+// @title Go Clean Architecture API
+// @version 1.0
+// @description This is a sample server for a Go application following Clean Architecture principles.
+// @host localhost:8080
+// @BasePath /api/v1
+
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	_ "github.com/Bobby-P-dev/go-clean-api.git/docs"
+
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	api := r.Group("/api/v1")
+	{
+		api.GET("/health", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"sucsses": true,
+				"message": "API is running",
+			})
+		})
+	}
+	r.Run(":8080")
+}
---
+++ pkg/response/response.go
@@ -0,0 +1,22 @@
+package response
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Success(c *gin.Context, message string, data interface{}) {
+	c.JSON(http.StatusOK, gin.H{
+		"success": true,
+		"message": message,
+		"data":    data,
+	})
+}
+
+func Error(c *gin.Context, statusCode int, message string) {
+	c.JSON(statusCode, gin.H{
+		"success": false,
+		"message": message,
+	})
+}
